713f46260dc5d4178b13f585c31748441655f2fc
yamamoto <yamashu0506@gmail.com>
2019-10-23 10:39:03 +0900

    yamamoto plan_check js commit

--- www/js/plan_check.js
+++ www/js/plan_check.js
@@ -1,24 +1,17 @@
-$(function(){
-	document.addEventListener('init', function(event) {
-		var page= event.target;
-		//質問ページの時のみ処理
-		if(page.matches('#plan_check')){
-			//データ取得
-			var history_type=page.data.history_type;
-			let data=history_array[history_type];
-			let elem="";
-			//スケジュール出力
-			$("#check_plan_list").html(
-				"<ons-list-item>8時30分～9時00分 : 電車○○駅発</ons-list-item>"+
-				"<ons-list-item>9時15分～10時30分 : </ons-list-item>"
-			);
-			for(let i=0;i<data["data"].length;i++){
-				elem+="<ons-list-item>"+data["data"][i]["startName"]+"～"
-					+data["data"][i]["endName"]+"　　"
-					+data["data"][i]["time_ja"]+"</ons-list-item>";
-			}
-			$("#check_plan_list").html(elem);
-
+document.addEventListener('init', function (event) {
+	var page = event.target;
+	//質問ページの時のみ処理
+	if (page.matches('#plan_check')) {
+		//データ取得
+		var history_type = page.data.history_type;
+		let check_data = history_array[history_type];
+		//スケジュール出力
+		let elem = "";
+		for (let i = 0; i < check_data["data"].length; i++) {
+			elem += "<ons-list-item  modifier='chevron' tappable>" + check_data["data"][i]["startPoint"]["name"] + "～"
+				+ check_data["data"][i]["endPoint"]["name"] + "　　"
+				+ check_data["data"][i]["time_ja"] + "</ons-list-item>";
 		}
-	});
+		$("#check_plan_list").html(elem);
+	}
 });
