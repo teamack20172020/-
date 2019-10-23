07c637b1322fdee795380b8d06b2f6e3e6d6403a
yamamoto <yamashu0506@gmail.com>
2019-10-23 10:14:39 +0900

    yamamoto generation js commit

--- www/js/generation.js
+++ www/js/generation.js
@@ -1,69 +1,70 @@
 //ローカルストレージに保存するために使う変数
-var generation_array=Array();
+var generation_array = new Array();
+//自動生成APIのURL「travelplan/create/」
+var ge_url = "travelplan/create/";
 //作成したプランを一時的に保存する変数
-var generation_work=Array();
-//引数（緯度、経度、出発地タイプ、目的,目的地,URL）
-function generation_ajax(url){
-	$.ajax({
-		type: "GET",
-		//url: "js/sample.json",
-		url: "https://www.autotravelplan.com/" + url,
-		dataType:"json",
-		scriptCharset: "utf-8",
-		timeout: 30000
-	}).done(function(data){
-		console.log(data);
-		//今日の日付を取得
-		let today=new Date();
-		generation_work=data;
-		generation_work["create_date"]=today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
-		generation_work["create_time"]=today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
-		console.log(generation_work);
-		var elem="";
-		for(let i=0;i<data.length;i++){
-			elem+="<ons-list-item>"+data[i]["startPoint"]["name"]+"～"
-				+data[i]["endPoint"]["name"]+"　　"
-				+data[i]["time_ja"]+"</ons-list-item>";
-		}
-		$("#gene_plan_list").html(elem);
-		$('#modal').hide();
-	});
-}
-document.addEventListener('show', function(event) {
-	var page= event.target;
+var g_work = {};
+//画面が読み込まれたときの処理
+document.addEventListener('show', function (event) {
+	g_work = {};
+	var page = event.target;
 	//質問ページの時のみ処理
-	if(page.matches('#generation')){
+	if (page.matches('#generation')) {
 		//スケジュール生成
-		$("#gene_purpose").text("目的："+purpose);
-		$("#gene_departure").text("出発地："+lat+lng);
-		$("#gene_plan_list").html(
-			"<ons-list-item>8時30分～9時00分 : 電車○○駅発</ons-list-item>"+
-			"<ons-list-item>9時15分～10時30分 : </ons-list-item>"
-		);
-		//ajax();
-		generation_ajax("travelplan/create/"+departure_type+"/"+lat+","+lng+"/"+purpose+"/37");
-		//読み込み中画面を閉じる
+		$("#gene_purpose").text("目的：" + purpose);
+		$("#gene_departure").text("出発地：" + departure_type);
+		
+		ajax(ge_url + departure_type + "/" + lat + "," + lng + "/" + purpose + "/37", "generation_auto", "in", "json");
+		//generation_ajax("travelplan/create/" + departure_type + "/" + lat + "," + lng + "/" + purpose + "/37");
 
 	}
 });
-//「もう一度自動生成する」ボタンクリック
-$(document).on("click","#again_plan",function(){
-	$('#modal').show();
-	document.getElementById("main").resetToPage('generation.html',{animation:'slide-ios'});
-});
-//「完了」ボタンクリック
-$(document).on("click","#complete_plan",function(){
-	//ローカルストレージから取得
-	if(getLocalStorage("generation")!=null){
-		generation_array=getLocalStorage("generation");
+
+//プラン自動生成ajax通信の結果退避
+function setResG(resg) {
+	console.log(resg);
+	//今日の日付を取得
+	let today = new Date();
+	g_work["data"] = resg;
+	g_work["create_purpose"] = purpose;
+	g_work["create_date"] = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+	g_work["create_time"] = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	viewGeneration(resg);
+}
+
+//自動生成したプランを表示する
+function viewGeneration(viewg) {
+	var elem = "";
+	for (let i = 0; i < viewg.length; i++) {
+		elem += "<ons-list-item>" + viewg[i]["startPoint"]["name"] + "～"
+			+ viewg[i]["endPoint"]["name"] + "　　"
+			+ viewg[i]["time_ja"] + "</ons-list-item>";
 	}
-	generation_array.push(generation_work);
-	setLocalStorage("generation",generation_array);
+	$("#gene_plan_list").html(elem);
+	//読み込み中画面を閉じる
+	$('#modal').hide();
+}
+
+//「もう一度自動生成する」ボタンクリック
+$(document).on("click", "#again_plan", function () {
+	$('#modal').show();
+	document.getElementById("main").resetToPage('generation.html', { animation: 'slide-ios' });
+});
+
+//「完了」ボタンクリック
+$(document).on("click", "#complete_plan", function () {
+	//ローカルストレージから取得
+	if (getLocalStorage("generation") != null) {
+		generation_array = getLocalStorage("generation");
+	}
+	generation_array.push(g_work);
+	setLocalStorage("generation", generation_array);
 	console.log(generation_array);
 	alert("履歴に登録しました");
-	document.getElementById("main").resetToPage('home.html',{animation:'slide-ios'});
+	document.getElementById("main").resetToPage('home.html', { animation: 'slide-ios' });
 });
+
 //「キャンセル」ボタンクリック
-$(document).on("click","#cancel_plan",function(){
-	document.getElementById("main").resetToPage('home.html',{animation:'slide-ios'});
+$(document).on("click", "#cancel_plan", function () {
+	document.getElementById("main").resetToPage('home.html', { animation: 'slide-ios' });
 });
