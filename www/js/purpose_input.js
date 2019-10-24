c03d3824b0ddfddf7b39cd049338d6ec04d9f187
IS1-HAMAMOTO\hamamoto <hamayuu686@gmail.com>
2019-10-24 14:23:57 +0900

    purpose_input.jsの最適化

--- www/js/purpose_input.js
+++ www/js/purpose_input.js
@@ -1,5 +1,4 @@
-﻿
-//変数定義
+﻿//変数定義
 var id;
 var answer;
 var res =new Array();
@@ -9,10 +8,10 @@ var objectiveList =new Array();
 //画面ロード直後の処理
 document.addEventListener('init', function(event) {
 	var page= event.target;
-	//質問ページの時のみ処理
+	//目的入力ページの時のみ処理
 	if(page.matches('#purpose_input')){
-		var url = "objective/getList";
-		var list = ajax(url,"purpose","in","json");
+		var purposeurl = "objective/getList";
+		ajax(purposeurl,"purpose","in","json");
 	}
 });
 
@@ -27,6 +26,7 @@ function setResP(resList){
 	console.log(idlist);
 	viewPurpose();
 }
+
 //目的セレクトボックスを表示
 function viewPurpose(){
 	var option = "";
@@ -37,6 +37,7 @@ function viewPurpose(){
 	$("#purpose_select select").html(option);
 	$("#modal").hide();
 }
+
 //目的決定ボタンクリック処理
 $(document).on("click","#submit_purpose",function(){
 	purpose=$("#purpose_select").val();
