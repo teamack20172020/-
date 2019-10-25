6f66857562022cef12dd47e375c67d3398e3cbb5
yamamoto <yamashu0506@gmail.com>
2019-10-25 10:37:06 +0900

    yamamoto plan_check js commit

--- www/js/plan_check.js
+++ www/js/plan_check.js
@@ -1,5 +1,11 @@
 //一つのプランが格納される変数
 var check_data;
+//ブラウザのgoogle mapを表示する為の変数
+var googlemapurl ="https://www.google.co.jp/maps/dir/";
+
+//monacaでブラウザを表示する為の処理
+document.addEventListener("deviceready", onDeviceReady, false);
+
 document.addEventListener('init', function (event) {
 	var page = event.target;
 	//プラン確認ページの時のみ処理
@@ -10,7 +16,7 @@ document.addEventListener('init', function (event) {
 		//スケジュール出力
 		let elem = "";
 		for (let i = 0; i < check_data.length; i++) {
-			elem += "<ons-list-item  modifier='chevron' class='check_item' value='" + i + "' tappable>" + check_data[i]["startPoint"]["name"] + "～"
+			elem += "<ons-list-item modifier='chevron' class='check_item' value='" + i + "' tappable>" + check_data[i]["startPoint"]["name"] + "～"
 				+ check_data[i]["endPoint"]["name"] + "　　"
 				+ check_data[i]["time_ja"] + "</ons-list-item>";
 		}
@@ -18,9 +24,17 @@ document.addEventListener('init', function (event) {
 	}
 });
 
-//プランクリック時の処理
-$(document).on("click", ".check_item", function () {
-	let check_type = $(this).attr("value");
-	let work_check = check_data[check_type];
-	document.getElementById('main').pushPage("plan_detail.html", { data: { work_check } });
-});
+//monacaでブラウザを表示する為の処理内容
+function onDeviceReady() {
+	console.log("window.open works well");
+	//プランクリック時の処理
+	$(document).on("click", ".check_item", function () {
+		//チェックされた項目を取得
+		let check_type = $(this).attr("value");
+		let work_check = check_data[check_type];
+		//ブラウザを表示
+		var ref = cordova.InAppBrowser.open(googlemapurl + work_check['startPoint']["name"] + "/" + work_check['endPoint']["name"], '_blank', 'location=yes');
+		// document.getElementById('main').pushPage("plan_detail.html", { data: { work_check } });
+	});
+}
+
