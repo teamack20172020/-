1a88e4e0bed23c2d77fbea677b5042da84012ffd
yamamoto <yamashu0506@gmail.com>
2019-11-18 12:17:58 +0900

    yahoomap.jsのコンソール整理

--- www/js/yahoomap.js
+++ www/js/yahoomap.js
@@ -3,24 +3,26 @@ var ymap;
 //yahooMAPマーカー
 var marker;
 //y:経度（縦）、x:経度（横）
-function Ymap(y,x){
-	lat=y;
-	lng=x;
-	departure_type="現在位置";
-	console.log("緯度:"+lat+",経度:"+lng);
+function Ymap(y, x) {
+	lat = y;
+	lng = x;
+	departure_type = "現在位置";
+	//ヤフーマップに出す真ん中の緯度経度を出すコンソール
+	//console.log("緯度:" + lat + ",経度:" + lng);
 	ymap = new Y.Map("map");
 	ymap.drawMap(new Y.LatLng(y, x), 16, Y.LayerSetId.NORMAL);
-	marker = new Y.Marker(new Y.LatLng(y,x));
+	marker = new Y.Marker(new Y.LatLng(y, x));
 	ymap.addFeature(marker);
 	//マップ内をクリック時緯度経度の更新処理
-	ymap.bind('click', function(latlng){
-   		var work=latlng.toString().split(",");
+	ymap.bind('click', function (latlng) {
+		var work = latlng.toString().split(",");
 		//緯度経度を設定
 		ymap.removeFeature(marker);
-		marker= new Y.Marker(new Y.LatLng(parseFloat(work[0]),parseFloat(work[1])));
+		marker = new Y.Marker(new Y.LatLng(parseFloat(work[0]), parseFloat(work[1])));
 		ymap.addFeature(marker);
-		lat=parseFloat(work[0]);
-		lng=parseFloat(work[1]);
-		console.log("緯度:"+lat+",経度:"+lng);
+		lat = parseFloat(work[0]);
+		lng = parseFloat(work[1]);
+		//クリックした緯度経度を出すコンソール
+		//console.log("緯度:" + lat + ",経度:" + lng);
 	});
 }
