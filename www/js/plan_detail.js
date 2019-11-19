4bef55deb3598578a366186f3c2c5347a4947df5
yamamoto <yamashu0506@gmail.com>
2019-11-19 14:33:54 +0900

    plan_detail.jsに詳細画面のマップ表示とブラウザ表示を追加

--- www/js/plan_detail.js
+++ www/js/plan_detail.js
@@ -1,43 +1,22 @@
+//1施設情報が入っている配列
+var de_array;
+
 document.addEventListener('init', function (event) {
 	var page = event.target;
 	if (page.matches('#plan_detail')) {
-		let check_data = page.data.work_check;
-		let work_time = check_data['time_second']/60;
-		console.log(check_data);
-		planMap(check_data["startPoint"]["lat"], check_data["startPoint"]["lng"]
-			, check_data["endPoint"]["lat"], check_data["endPoint"]["lng"], work_time);
+		de_array = page.data.de_work;
+		//施設情報が入っている配列
+		//console.log(de_array);
+		pd_Ymap(de_array[0]["lat"],de_array[0]["lng"]);
+		$("#pd_name").html(de_array["name"]);
+		$("#pd_url").html("<a href='#' id='detail_url'>" + de_array["site-url"]+"</a>");
+		$("#pd_tel").html(de_array["number"]);
 	}
 });
 
-//プランの経路探索マップ
-function planMap(Slat, Slng, Glat, Glng, time) {
-	console.log(time);
-	let center_lat = (Slat + Glat) / 2;
-	let center_lng = (Slng + Glng) / 2;
-	let pd_ymap = new Y.Map("pd_map");
-	let mapsize = 1;
-	if (time >= 50.0) {
-		mapsize = 10;
-	} else if (time >= 40.0) {
-		mapsize = 11;
-	} else if (time >= 30.0) {
-		mapsize = 12;
-	} else if (time >= 20.0) {
-		mapsize = 12;
-	} else if (time >= 10.0) {
-		mapsize = 14;
-	} else {
-		mapsize = 15;
-	}
-	pd_ymap.drawMap(new Y.LatLng(center_lat, center_lng), mapsize, Y.LayerSetId.NORMAL);
-	var latlngs = [
-		new Y.LatLng(Slat, Slng),  //スタート位置
-		new Y.LatLng(Glat, Glng),  //ゴール位置
-	];
-	var config = {
-		"traffic": "car"
-	};
-	var pd_layer = new Y.RouteSearchLayer();
-	pd_ymap.addLayer(pd_layer);
-	pd_layer.execute(latlngs, config);
-}
+$(document).on("click","#detail_url",function(){
+	let de_url=$(this).text();
+	console.log(de_url);
+	var ref = cordova.InAppBrowser.open(de_url, 
+		'_blank', 'location=no,closebuttoncaption=戻る,toolbarposition=top');
+});
