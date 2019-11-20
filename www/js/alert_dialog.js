47374cbba61ded440503ec52b359841c2d5003a0
yamamoto <yamashu0506@gmail.com>
2019-11-20 15:05:28 +0900

    alert_dialog.jsの自動生成画面で使うアラートの処理を追加

--- www/js/alert_dialog.js
+++ www/js/alert_dialog.js
@@ -20,6 +20,26 @@ function removePlan(name){
 
 //generation.jsの完了ボタンがクリックされてアラート出た時の処理
 function ge_ok(){
+	//タイトル情報取得
+	let p_title = $("#plan_title_in").val();
+	//ローカルストレージから取得
+	if (getLocalStorage("generation") != null) {
+		generation_array = getLocalStorage("generation");
+	}
+	let nen = g_work["create_date"].split("-");
+	let jikan=g_work["create_time"].split(":");
+	if(p_title.length<=0 || p_title==null){
+		p_title = nen[0] + "年" + nen[1] + "月" + nen[2] + "日 " + jikan[0] + "時" + jikan[1]+"分に生成したプラン";
+	}
+	//generation.jsにあるg_workにタイトルを追加してローカルストレージに保存する
+	g_work["title"]=p_title;
+	console.log(g_work);
+	generation_array.push(g_work);
+	//ローカルストレージに生成したプランを保存
+	setLocalStorage("generation", generation_array);
+	//ローカルストレージに保存した配列を出すコンソール
+	//console.log(generation_array);
+	$("#plan_title_in").val("");
 	$("#generation_ok").hide();
 	document.getElementById("main").resetToPage('home.html', { animation: 'slide-ios' });
 }
