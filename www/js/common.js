eefebe6553fde1213ef3821a9300cad7fb23f507
yamamoto <yamashu0506@gmail.com>
2019-11-27 10:40:33 +0900

    ソースを少し見やすくした

--- www/js/common.js
+++ www/js/common.js
@@ -2,12 +2,12 @@
 var send_array = null;
 
 //ページ遷移
-$(function(){
+$(function () {
 	rotes = new AppRotes();
 	//nextpageクラスをタップした時次のページへ遷移する処理
-	$(document).on("click",".nextpage",function(){
+	$(document).on("click", ".nextpage", function () {
 		nextpage = rotes.getRote($(this).attr("id"));
-		if(nextpage != ""){
+		if (nextpage != "") {
 			//次のページに移動
 			document.getElementById("main").pushPage(nextpage + ".html");
 		}
@@ -25,13 +25,13 @@ function compareFunc(a, b) {
 
 //ローカルストレージへの保存
 function setLocalStorage(key, value) {
-	localStorage.setItem(key,JSON.stringify(value));
+	localStorage.setItem(key, JSON.stringify(value));
 }
 
 //ローカルストレージから取得
 function getLocalStorage(key) {
-	let work=localStorage.getItem(key);
-	work=JSON.parse(work);
+	let work = localStorage.getItem(key);
+	work = JSON.parse(work);
 	return work;
 }
 
@@ -44,13 +44,42 @@ $(document).on("click", ".detail_item", function () {
 });
 
 //strの文字を8の数までの文字列にして返すメソッド
-function count_text(str){
-	let work=str;
-	if(work.length>8){
-		work=str.slice(0,8);
-		work+="...";
+function count_text(str) {
+	let work = str;
+	if (work.length > 8) {
+		work = str.slice(0, 8);
+		work += "...";
 	}
 	//変換したworkをコンソールに出す
 	//console.log(work);
 	return work;
+}
+
+//クロスサイトスクリプティング対策メソッド
+function htmlspecialchars(ch) {
+	ch = ch.replace(/&/g, "&amp;");
+	ch = ch.replace(/"/g, "&quot;");
+	ch = ch.replace(/'/g, "&#039;");
+	ch = ch.replace(/</g, "&lt;");
+	ch = ch.replace(/>/g, "&gt;");
+	// ch = ch.replace(" ", "");
+	// ch = ch.replace("　", "");
+	ch = ch.replace("	", "");
+	return ch;
+}
+
+//引数でtypeイメージを返すメソッド
+function type_image(str) {
+	let result = "img/";
+	switch (str) {
+		case "1": result += "icon_sightseeing.png"; break;
+		case "2": result += "icon_gourmet.png"; break;
+		case "3": result += "icon_history.png"; break;
+		case "4": result += "icon_leisure.png"; break;
+		case "5": result += "icon_art.png"; break;
+		case "6": result += "icon_nature.png"; break;
+		case "7": result += "icon_spa.png"; break;
+		default: result += "icon_departure.png";break;
+	}
+	return result;
 }
