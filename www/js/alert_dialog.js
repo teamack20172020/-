92d7fdcf87ee39da9946b6c9be5da7246647e1f0
yamamoto <yamashu0506@gmail.com>
2019-12-10 09:38:02 +0900

    修正

--- www/js/alert_dialog.js
+++ www/js/alert_dialog.js
@@ -1,8 +1,8 @@
 //プラン一覧画面で削除するときのアラート表示処理
-function viewAlertHIS(){
+function viewAlertHIS() {
 	let view_text = "本当にこのプランを削除しても良いですか?※二度と同じプランは作成できないかもしれません";
 	$("#plan_remove_alert_text").html(view_text);
-	$("#plan_remove_alert_ok").attr("onclick","removePlan('his');");
+	$("#plan_remove_alert_ok").attr("onclick", "removePlan('his');");
 	//アラートを表示(alert_dialog.jsに処理は記載)
 	$("#remove-alert-dialog").show();
 }
@@ -18,20 +18,20 @@ function viewAlertCHE(re_num) {
 }
 
 //住所指定の出発地入力画面でアラートを出す処理
-function viewAlertADD(){
+function viewAlertADD() {
 	let view_text = "丁目番地号が入力されていません";
 	$("#alert_text").html(view_text);
 	$("#my-alert-dialog").show();
 }
 
 //アラートディアログのキャンセルボタンの処理
-function cancelAlert(name){
+function cancelAlert(name) {
 	$("#plan_title_in").val("");
-	$("#"+name).hide();
+	$("#" + name).hide();
 }
 
 //plan_historyの履歴を削除するメソッド
-function removePlan(type){
+function removePlan(type) {
 	//要素を削除した後の配列
 	let remove_after = history_array.splice(history_remove_num, 1);
 	//プランを削除した後の配列と削除する前の配列を出すコンソール
@@ -39,16 +39,16 @@ function removePlan(type){
 	//console.log(history_array);
 	setLocalStorage("generation", history_array);
 	$("#remove-alert-dialog").hide();
-	if(type=="his"){
+	if (type == "his") {
 		//plan_history.jsファイルのメソッド
 		viewHistory(1);
-	}else{
+	} else {
 		document.getElementById("main").popPage();
 	}
 }
 
 //generation.jsの完了ボタンがクリックされてアラート出た時の処理
-function ge_ok(){
+function ge_ok() {
 	//タイトル情報取得
 	let p_title = htmlspecialchars($("#plan_title_in").val());
 	//ローカルストレージから取得
@@ -56,13 +56,13 @@ function ge_ok(){
 		generation_array = getLocalStorage("generation");
 	}
 	let nen = g_work["create_date"].split("-");
-	let jikan=g_work["create_time"].split(":");
-	if(p_title.length<=0 || p_title==null){
-		p_title = nen[0] + "年" + nen[1] + "月" + nen[2] + "日　" + jikan[0] + "時" + jikan[1]+"分";
+	let jikan = g_work["create_time"].split(":");
+	if (p_title.length <= 0 || p_title == null) {
+		p_title = nen[0] + "年" + nen[1] + "月" + nen[2] + "日　" + jikan[0] + "時" + jikan[1] + "分";
 	}
 	//generation.jsにあるg_workにタイトルを追加してローカルストレージに保存する
-	p_title=getLen(p_title);
-	g_work["title"]=p_title;
+	p_title = getLen(p_title);
+	g_work["title"] = p_title;
 	//タイトルの入ったプランをコンソールに出す
 	//console.log(g_work);
 	generation_array.push(g_work);
@@ -72,8 +72,9 @@ function ge_ok(){
 	//console.log(generation_array);
 	$("#plan_title_in").val("");
 	$("#generation_ok").hide();
+
 	history_array = getLocalStorage("generation");
-	history_type=history_array.length-1;
+	history_type = history_array.length - 1;
 	his_work = history_array[history_type];
 	console.log(his_work);
 	document.getElementById('main').pushPage("plan_check.html", { data: { his_work, history_type }, animation: 'slide-ios' });
