b90cbbfbade44ca8cb4d38aa655381d62a20119a
yamamoto <yamashu0506@gmail.com>
2019-12-27 13:36:13 +0900

    最初の読み込みで失敗したときの処理

---
+++ www/js/title.js
@@ -0,0 +1,38 @@
+//ページがタイトル画面だった場合の処理
+document.addEventListener('init', function (event) {
+	var page = event.target;
+	if (page.matches('#title')) {
+		ss();
+	}
+});
+
+//目的リストを取得するメソッド
+function ss(){
+	//初期で目的一覧を取得する
+	var purposeurl = "objective/getList";
+	ajax(purposeurl, "purpose", "in", "json");
+}
+
+//ajax通信の結果退避
+function setResP(resList) {
+	resList.forEach(function (row) {
+		objectiveList[row["id"]] = row["title"];
+		objectiveAboutList[row["id"]] = row["about"];
+	});
+	idlist = Object.keys(objectiveList);
+	//目的IDを保存した配列を出すコンソール
+	//console.log(idlist);
+	document.getElementById("main").pushPage('home.html');
+}
+
+//ajax通信の失敗時の処理
+function setErrP(){
+	$("#title .answer_box").html('<ons-button id="re_connect">再接続</ons-button>');
+}
+
+//再接続ボタンクリック時の処理
+$(document).on("click", "#re_connect",function(){
+	$("#title .answer_box").html('<ons-icon icon="md-spinner" size="50px" spin></ons-icon>');
+	ss();
+});
+
